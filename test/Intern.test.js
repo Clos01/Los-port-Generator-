c515dbf71d0ea964dea770013ec2e7cbb04fa06d
Carlos Rivas <Cjrivas00@gmail.com>
2022-03-10 10:08:09 -0500

    intern test js complete

---
+++ test/Intern.test.js
@@ -0,0 +1,21 @@
+// (npm run test) this requiers it to through lib then intern to make sure the code works from there it tests the values to make sure it can give an output
+
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
